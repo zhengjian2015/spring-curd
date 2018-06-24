80b4c0d01d3663834d587729760c23f29fb3157b
zj21zhengj <570470242@qq.com>
2018-06-24 17:31:02 +0800

    datatable 初始化；excel导入

--- src/main/java/com/zj/curd/service/IDepartmentService.java
+++ src/main/java/com/zj/curd/service/IDepartmentService.java
@@ -2,8 +2,10 @@ package com.zj.curd.service;
 
 import java.util.List;
 
-import com.zj.curd.pojo.Department;
+import org.springframework.stereotype.Repository;
 
+import com.zj.curd.pojo.Department;
+@Repository
 public interface IDepartmentService  {
 	 public Department getDepartmentById(int departmentId); 
 	 
