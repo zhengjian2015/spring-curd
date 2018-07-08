e41343e85595d2286c7bb05ed4f41abff908256e
zj21zhengj <570470242@qq.com>
2018-07-08 16:56:05 +0800

    登陆界面

---
+++ src/main/java/com/zj/curd/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.zj.curd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login",method=RequestMethod.GET)  
+    public String getLogin(){  
+		
+        return "login/login";  
+    }
+
+}
